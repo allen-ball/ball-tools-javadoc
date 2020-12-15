f5efbc3fdb615805e4e841c3a2556755cf4f7574
Allen D. Ball <ball@hcf.dev>
2020-12-15 05:48:53 +0000

    Created jdk8 subdirectory.
    git-svn-id: svn+ssh://svn.hcf.dev/var/spool/scm/repository.svn/ball-tools-javadoc/trunk@7086 b3e4a033-f9ea-474d-ac63-5dcbede9114a

---
+++ jdk8/src/main/java/ball/tools/javadoc/StandardDoclet.java
@@ -0,0 +1,85 @@
+package ball.tools.javadoc;
+/*-
+ * ##########################################################################
+ * Utilities
+ * $Id$
+ * $HeadURL$
+ * %%
+ * Copyright (C) 2008 - 2020 Allen D. Ball
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ##########################################################################
+ */
+import com.sun.javadoc.DocErrorReporter;
+import com.sun.javadoc.RootDoc;
+import com.sun.tools.doclets.standard.Standard;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * {@link Standard} subclass which provides additional services to
+ * {@link AbstractTaglet} implementations.
+ *
+ * @author {@link.uri mailto:ball@hcf.dev Allen D. Ball}
+ * @version $Revision$
+ */
+@NoArgsConstructor @ToString
+public class StandardDoclet extends Standard {
+
+    /**
+     * See
+     * {@link com.sun.javadoc.Doclet#validOptions(String[][],DocErrorReporter)}.
+     */
+    public static String[][] options = null;
+
+    /**
+     * See
+     * {@link com.sun.javadoc.Doclet#validOptions(String[][],DocErrorReporter)}.
+     */
+    public static DocErrorReporter reporter = null;
+
+    /**
+     * See {@link com.sun.javadoc.Doclet#start(RootDoc)}.
+     */
+    public static RootDoc root = null;
+
+    /**
+     * See
+     * {@link com.sun.javadoc.Doclet#validOptions(String[][],DocErrorReporter)}.
+     *
+     * @param   options         The {@code options} and their arguments.
+     * @param   reporter        The {@link DocErrorReporter}.
+     *
+     * @return  {@code true} if the {@code options} are valid; {@code false}
+     *          otherwise.
+     */
+    public static boolean validOptions(String[][] options, DocErrorReporter reporter) {
+        StandardDoclet.options = options;
+        StandardDoclet.reporter = reporter;
+
+        return Standard.validOptions(options, reporter);
+    }
+
+    /**
+     * See {@link com.sun.javadoc.Doclet#start(RootDoc)}.
+     *
+     * @param   root            The {@link RootDoc}.
+     *
+     * @return  {@code true} on success; {@code false} otherwise.
+     */
+    public static boolean start(RootDoc root) {
+        StandardDoclet.root = root;
+
+        return Standard.start(root);
+    }
+}
